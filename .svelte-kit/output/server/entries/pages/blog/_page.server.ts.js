06571881c62eea052444b3cc3004f2dcf181d13d
Eve Lu <107573330+luyi-eve@users.noreply.github.com>
2025-03-02 16:28:42 -0500

    Delete .svelte-kit directory

--- .svelte-kit/output/server/entries/pages/blog/_page.server.ts.js
+++
@@ -1,8 +0,0 @@
-async function load({ fetch }) {
-  const response = await fetch("api/content");
-  const posts = await response.json();
-  return { posts };
-}
-export {
-  load
-};
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CqoC2MuK.js","_app/immutable/chunks/oaJrz1TM.js","_app/immutable/chunks/COxv0z7M.js","_app/immutable/chunks/KeRjE77N.js","_app/immutable/chunks/BWzIiUQM.js","_app/immutable/chunks/CYgJF_JY.js"];
-export const stylesheets = [];
-export const fonts = [];
